a7889d064d04e8032eefd01a11b682c46ba6d140
Ivan Frolov <frolvanya@gmail.com>
2024-12-03 19:53:08 -0500

    feat: determine method name by discriminator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 
+use anyhow::Result;
 use futures_util::StreamExt;
 
 use solana_client::nonblocking::{pubsub_client::PubsubClient, rpc_client::RpcClient};
@@ -8,7 +9,11 @@ use solana_client::rpc_config::{RpcTransactionLogsConfig, RpcTransactionLogsFilt
 use solana_client::rpc_response::RpcConfirmedTransactionStatusWithSignature;
 use solana_sdk::signature::Signature;
 use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey};
-use solana_transaction_status::UiTransactionEncoding;
+use solana_transaction_status::{UiMessage, UiRawMessage, UiTransactionEncoding};
+use solana_transaction_status_client_types::UiTransaction;
+
+const FINALIZE_DEPOSIT_DISCRIMINATOR: [u8; 8] = [240, 178, 165, 14, 221, 29, 104, 47];
+const FINALIZE_WITHDRAW_DISCRIMINATOR: [u8; 8] = [17, 72, 11, 172, 214, 42, 12, 23];
 
 #[tokio::main]
 async fn main() {
@@ -44,10 +49,7 @@ async fn main() {
     }
 }
 
-async fn fetch_recent_logs(
-    http_url: &str,
-    program_account: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
+async fn fetch_recent_logs(http_url: &str, program_account: &str) -> Result<()> {
     let rpc_client = RpcClient::new(http_url.to_string());
     let pubkey = Pubkey::from_str(program_account)?;
 
@@ -58,7 +60,7 @@ async fn fetch_recent_logs(
             .get_signatures_for_address_with_config(
                 &pubkey,
                 GetConfirmedSignaturesForAddress2Config {
-                    limit: Some(1000),
+                    limit: None,
                     before: last_signature,
                     until: None,
                     commitment: Some(CommitmentConfig::confirmed()),
@@ -77,8 +79,14 @@ async fn fetch_recent_logs(
                 .get_transaction(&signature, UiTransactionEncoding::Json)
                 .await
             {
-                if let Some(meta) = tx.transaction.meta {
-                    println!("Transaction details: {:?}", meta.log_messages);
+                let transaction = tx.transaction.transaction;
+                match transaction {
+                    solana_transaction_status::EncodedTransaction::Json(tx) => {
+                        print_tx_info(&tx, signature)?;
+                    }
+                    _ => {
+                        eprintln!("Unsupported transaction encoding");
+                    }
                 }
             }
         }
@@ -89,4 +97,44 @@ async fn fetch_recent_logs(
     }
 
     Ok(())
+}
+
+fn print_tx_info(tx: &UiTransaction, signature: Signature) -> Result<()> {
+    println!();
+    println!("Transaction Signature: {}", signature);
+
+    match tx.message {
+        UiMessage::Parsed(_) => panic!("Unsupported transaction encoding"),
+        UiMessage::Raw(ref raw) => print_raw_message(raw),
+    }
+
+    Ok(())
+}
+
+fn print_raw_message(message: &UiRawMessage) {
+    println!("Transaction Message (raw):");
+    println!("  Account Keys: {:?}", message.account_keys);
+    println!("  Recent Blockhash: {:?}", message.recent_blockhash);
+
+    for instruction in message.instructions.clone() {
+        let index = instruction.program_id_index as usize;
+        let program_id = message.account_keys[index].clone();
+        let method = decode_instruction(&instruction.data);
+
+        println!("Program ID: {}, Method: {:?}", program_id, method);
+    }
+}
+
+fn decode_instruction(data: &str) -> Option<String> {
+    let decoded_data = bs58::decode(data).into_vec().ok()?;
+    println!("Raw Data: {:?}", data);
+    println!("Decoded Data: {:?}", decoded_data);
+
+    if decoded_data.starts_with(&FINALIZE_DEPOSIT_DISCRIMINATOR) {
+        Some("finalize_deposit".to_string())
+    } else if decoded_data.starts_with(&FINALIZE_WITHDRAW_DISCRIMINATOR) {
+        Some("finalize_withdraw".to_string())
+    } else {
+        None
+    }
 }
